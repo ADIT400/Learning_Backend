0ec7e50ee7f6fbed5e7d0049bd267c209bcd850f
ADIT400 <adityadangi1920@gmail.comm>
2025-09-26 21:45:59 +0530

    Learned about backend basics,node.js,npm(system operation),NPM basics and advanced features,Express.js,Routing and Middleware, Form handlng,cookies,sessions,EJS ,Dynamic routing, and working on a web texteditor website.

---
+++ script.js
@@ -0,0 +1,76 @@
+/*async function ab(){
+var blob=await fetch('https://randomuser.me/api/');
+var ans=await blob.json();
+console.log(ans.results[0].name);
+}
+ab();*/
+
+
+//const fs = require('fs');
+/*fs.writeFile("a.txt","Hello, Aditya!",function(err){
+    if(err) console.log(err);
+    else console.log("Done");
+})*/
+
+/*fs.appendFile("a.txt","\nWhat's up?",function(err){
+    if(err) console.log(err);
+    else console.log("Done");
+})*/
+
+/*fs.rename("b.txt","c.txt",function(err){
+    if(err) console.log(err);
+    else console.log("Done");
+})*/
+
+/*fs.copyFile("a.txt","./copy/copy.txt",function(err)
+{
+    if(err) console.log(err);
+    else console.log("Done");
+})*/
+
+/*fs.unlink("copy.txt",function(err)
+{
+    if(err) console.log(err);
+    else console.log("Done");
+})*/
+
+/*fs.rmdir("./copy",{recursive:true},function(err){
+    if(err) console.log(err);
+    else console.log("Done");
+})*/
+
+/*const http=require('http');
+const server=http.createServer(function(req,res){
+    res.end("Hello, Aditya!");
+})
+server.listen(3000);*/
+
+//console.log("Hello, Aditya!");
+
+/*import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(3000)*/
+
+const express=require('express');
+const app=express();
+
+app.use(function(req,res,next){
+    console.log("Middleware");
+    next(); 
+})
+
+app.get("/",function(req,res){
+    res.send("Hi, Whats up?")
+})
+
+/*app.get("/profile",function(req,res){
+    res.send("I am Aditya Dangi.")
+})*/
+
+app.listen(3000);
