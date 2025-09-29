dad51d0ac1fbe16152636aa7297b6b625946dabd
ADIT400 <adityadangi1920@gmail.comm>
2025-09-29 22:30:45 +0530

    Completed CRUD operations with MongoDB.

--- MongoDB/app.js
+++ MongoDB/app.js
@@ -9,11 +9,26 @@ app.get('/',(req,res)=>{
 
 app.get('/create',async(req,res)=>{
     let createduser=await userModel.create({
-        name:"Aditya",
-        email:"adityadangi1920@gmail.com",
-        username:"Aditya"
+        name:"Adit",
+        email:"aditdangi1920@gmail.com",
+        username:"Adit"
     })
     res.send(createduser);
+})
+
+app.get('/update',async(req,res)=>{
+    let updateduser=await userModel.findOneAndUpdate({username:"Aditya"},{name:"Aditya Dangi"},{new:true})
+    res.send(updateduser);
+})
+
+app.get('/read',async(req,res)=>{
+    let users=await userModel.find();
+    res.send(users);
+})
+
+app.get('/delete',async(req,res)=>{
+    let users=await userModel.findOneAndDelete({username:"Aditya"});
+    res.send(users);
 })
 
 app.listen(3000);
