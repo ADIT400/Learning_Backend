5abed5267b6b778faffe0ea2a194428c2d0fc14d
ADIT400 <adityadangi1920@gmail.comm>
2025-09-30 23:53:37 +0530

    Completed CRUD operations with EJS and Server-Side Rendering.

--- MongoDB/app.js
+++ MongoDB/app.js
@@ -1,4 +1,6 @@
-const express=require('express');
+//CRUD operations with MongoDB
+
+/*const express=require('express');
 const app=express();
 
 const userModel=require('./usermodel');
@@ -29,6 +31,51 @@ app.get('/read',async(req,res)=>{
 app.get('/delete',async(req,res)=>{
     let users=await userModel.findOneAndDelete({username:"Aditya"});
     res.send(users);
+})
+
+app.listen(3000);*/
+
+
+
+//CRUD operations with EJS and Server-Side Rendering
+
+const express=require('express');
+const app=express();
+const path=require('path');
+const userModel=require('./models/user');
+
+app.set("view engine","ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public'))); 
+
+app.get('/',(req,res)=>{
+    res.render("index");
+})
+
+app.get('/read',async(req,res)=>{
+    let users=await userModel.find();
+    res.render("read",{users});
+})
+
+app.get('/edit/:userid',async(req,res)=>{
+    let user=await userModel.findOne({_id: req.params.id});
+    res.render("edit",{user});
+})
+
+app.get('/delete/:id',async(req,res)=>{
+    let users=await userModel.findOneAndDelete({_id: req.params.id});
+    res.redirect("/read");
+})
+
+app.post('/create',async(req,res)=>{
+    let {name,email,image}=req.body;
+    let createduser=await userModel.create({
+        name,
+        email,
+        image
+    });
+    res.redirect("/read"); 
 })
 
 app.listen(3000);
--- MongoDB/usermodel.js
+++ MongoDB/usermodel.js
@@ -1,4 +1,6 @@
-const mongoose =require('mongoose');
+//CRUD operations with MongoDB
+
+/*const mongoose =require('mongoose');
 
 mongoose.connect(`mongodb://127.0.0.1:27017/mongopractice`);
 
@@ -8,4 +10,4 @@ const userSchema=mongoose.Schema({
     email:String
 })
 
-module.exports=mongoose.model("user",userSchema);
+module.exports=mongoose.model("user",userSchema);*/
