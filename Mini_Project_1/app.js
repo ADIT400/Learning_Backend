00a4a0b0aa86abdf2b6970b6b6b685ce2f59fdfa
ADIT400 <adityadangi1920@gmail.comm>
2025-10-05 23:57:15 +0530

    Working on my mini project. Today i am working on my profile page where you can see user info , u can logout from your id , you can create a post and can also checck th posts.

--- Mini_Project_1/app.js
+++ Mini_Project_1/app.js
@@ -19,11 +19,23 @@ app.get('/login',(req,res)=>{
     res.render("login");
 })
 
-app.get('/profile',isLoggedIn,(req,res)=>{
-    console.log(req.user);
-    
-    res.render("login");
-})
+app.get('/profile',isLoggedIn,async (req,res)=>{
+    let user=await userModel.findOne({email:req.user.email});
+    console.log(user);
+    res.render("profile",{user});
+});
+
+app.post('/post',isLoggedIn,async (req,res)=>{
+    let user=await userModel.findOne({email:req.user.email});
+    let {content}=req.body;
+    let post=await postModel.create({
+        user:user._id,
+        content
+    });
+    user.posts.push(post._id);
+    await user.save();
+    res.redirect("/profile");
+});
 
 app.post('/register',async (req,res)=>{
     let {email,username,name,age,password}=req.body;
@@ -57,7 +69,7 @@ app.post('/login',async (req,res)=>{
         if(result){ 
             let token=jwt.sign({email,userid:user._id},"abc");
             res.cookie("token",token);
-            res.status(200).send("You can login");
+            res.status(200).redirect("/profile");
         }
         else res.redirect("/login");
     })
@@ -69,9 +81,10 @@ app.get('/logout',(req,res)=>{
 })
 
 function isLoggedIn(req,res,next){
-    if(req.cookies.token==="") res.send("You must be logged in");
+    const token = req.cookies.token;
+    if(!token) res.redirect("/login");
     else{
-        let data=jwt.verify(req.cookies.token,"abc");
+        let data=jwt.verify(token,"abc");
         req.user=data;
         next();
     }
